5873382464bc609d900c5f2e705a05308ebe3c3d
alexanderpan <a0912716568@gmail.com>
2020-01-07 13:57:57 +0800

    copyright reserved

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,26 @@
           <b>Alexander</b> Pan
         </a>
         <div style="margin-left:auto; margin-right:0">
-          <a href='https://www.facebook.com/a0912716568' target="_blank"> <v-btn icon small style="padding-right:8px;">
-            <v-icon>{{fbLogo}}</v-icon>
-          </v-btn></a>
-          <a href='https://www.instagram.com/alexanderpanboy' target="_blank"> <v-btn icon small style="padding-right:8px;">
-            <v-icon>{{igLogo}}</v-icon>
-          </v-btn></a>
-          <a href='https://github.com/alexanderpanboy' target="_blank"><v-btn icon small style="padding-right:8px;">
-            <v-icon>{{gitHubLogo}}</v-icon>
-          </v-btn></a>
-          <a href='https://www.linkedin.com/in/alexander-a-pan-444755151' target="_blank"><v-btn icon small style="padding-right:8px;">
-            <v-icon>{{linkedInLogo}}</v-icon>
-          </v-btn></a>
+          <a href='https://www.facebook.com/a0912716568' target="_blank">
+            <v-btn icon small style="padding-right:18px; padding-left:8px;">
+              <v-icon>{{fbLogo}}</v-icon>
+            </v-btn>
+          </a>
+          <a href='https://www.instagram.com/alexanderpanboy' target="_blank">
+            <v-btn icon small style="padding-right:18px; padding-left:8px;">
+              <v-icon>{{igLogo}}</v-icon>
+            </v-btn>
+          </a>
+          <a href='https://github.com/alexanderpanboy' target="_blank">
+            <v-btn icon small style="padding-right:18px; padding-left:8px;">
+              <v-icon>{{gitHubLogo}}</v-icon>
+            </v-btn>
+          </a>
+          <a href='https://www.linkedin.com/in/alexander-a-pan-444755151' target="_blank">
+            <v-btn icon small style="padding-right:18px; padding-left:8px;">
+              <v-icon>{{linkedInLogo}}</v-icon>
+            </v-btn>
+          </a>
         </div>
 
         <!-- Float links to the right. Hide them on small screens -->
@@ -31,11 +39,7 @@
     </div>
 
     <!-- Header -->
-    <header
-      class="w3-display-container w3-content w3-wide w3-animate-zoom"
-      style="max-width:1500px;"
-      id="home"
-    >
+    <header class="w3-display-container w3-content w3-wide w3-animate-zoom" style="max-width:1500px;" id="home">
       <img class="w3-image" src="/assets/mainPage.jpg" alt="MainPage" width="1500" height="800" />
       <div class="w3-display-middle w3-margin-top w3-center">
         <h1 class="w3-xxlarge w3-text-white">
@@ -99,10 +103,14 @@
       <!-- About Section -->
       <div class="w3-container w3-padding-32" id="about">
         <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">About</h3>
-        <p>Hi there! I combine my unique design experiences into a tool kit that aids my clients in everything from branding to web design. As an HCI student, I am working to integrate UX methodology and research into my work as a designer to create beautiful and meaningful visual experiences. My interests include data visualization, visual design psychology, and micro-interaction design. You'll often find me drinking iced Americano (year-round) at a local coffee shop, poorly tending to my succulents, and playing with film camera.</p>
+        <p>Hi there! I combine my unique design experiences into a tool kit that aids my clients in everything from
+          branding to web design. As an HCI student, I am working to integrate UX methodology and research into my work
+          as a designer to create beautiful and meaningful visual experiences. My interests include data visualization,
+          visual design psychology, and micro-interaction design. You'll often find me drinking iced Americano
+          (year-round) at a local coffee shop, poorly tending to my succulents, and playing with film camera.</p>
       </div>
 
-      <div class="w3-row-padding w3-redscale">
+      <div class="w3-row-padding w3-redscal">
         <div class="w3-col l3 m6 w3-margin-bottom">
           <div>
             <img src="/assets/alexProfilePic.jpg" alt="Alex" style="width:100%" />
@@ -111,14 +119,17 @@
           <p class="w3-opacity">CEO & Founder</p>
           <p>
             I'm a Designer with a passion for effective design that helps both the client and user achieve their goals.
-            I'm currently pursuing a Master's degree in Human-Computer Interaction and Design at UC Irvine while working as an intern full stack developer at GIGABYTE Technology. Feel free to take a look at some of my work.
+            I'm currently pursuing a Master's degree in Human-Computer Interaction and Design at UC Irvine while working
+            as an intern full stack developer at GIGABYTE Technology. Feel free to take a look at some of my work.
           </p>
           <p>
-            <button class="w3-button w3-light-grey w3-block">Contact</button>
-            <a href='https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3Ab7d88a64-e0a7-4615-8b49-83fc18d28f35' target="_blank"><button class="w3-button w3-light-grey w3-block">Resume</button></a>
+            <button style="margin-bottom:3px;" class="w3-button w3-dark-grey w3-block">Contact</button>
+            <a href='/assets/APAN.pdf' target="_blank"><button
+                class="w3-button w3-dark-grey w3-block">Resume</button></a>
           </p>
         </div>
       </div>
+  
 
       <!-- Contact Section -->
       <div class="w3-container w3-padding-32" id="contact">
@@ -126,27 +137,9 @@
         <p>Lets get in touch and talk about your next project.</p>
         <form action="/action_page.php" target="_blank">
           <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name" />
-          <input
-            class="w3-input w3-section w3-border"
-            type="text"
-            placeholder="Email"
-            required
-            name="Email"
-          />
-          <input
-            class="w3-input w3-section w3-border"
-            type="text"
-            placeholder="Subject"
-            required
-            name="Subject"
-          />
-          <input
-            class="w3-input w3-section w3-border"
-            type="text"
-            placeholder="Comment"
-            required
-            name="Comment"
-          />
+          <input class="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email" />
+          <input class="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject" />
+          <input class="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment" />
           <button class="w3-button w3-black w3-section" type="submit">
             <i class="fa fa-paper-plane"></i> SEND MESSAGE
           </button>
@@ -162,55 +155,82 @@
     </div>
 
     <!-- Footer -->
-    <footer class="w3-center w3-black w3-padding-16">
-      <p>
-        Powered by
-        <a
-          href="https://www.w3schools.com/w3css/default.asp"
-          title="W3.CSS"
-          target="_blank"
-          class="w3-hover-text-green"
-        >w3.css</a>
-      </p>
-    </footer>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title class="w3-dark-grey">
+          <strong class="subheading">Get connected with me on social networks!</strong>
+          <div style="margin-left:auto; margin-right:0">
+            <a href='https://www.facebook.com/a0912716568' target="_blank">
+              <v-btn icon small style="padding-right:18px; padding-left:8px;">
+                <v-icon>{{fbLogo}}</v-icon>
+              </v-btn>
+            </a>
+            <a href='https://www.instagram.com/alexanderpanboy' target="_blank">
+              <v-btn icon small style="padding-right:18px; padding-left:8px;">
+                <v-icon>{{igLogo}}</v-icon>
+              </v-btn>
+            </a>
+            <a href='https://github.com/alexanderpanboy' target="_blank">
+              <v-btn icon small style="padding-right:18px; padding-left:8px;">
+                <v-icon>{{gitHubLogo}}</v-icon>
+              </v-btn>
+            </a>
+            <a href='https://www.linkedin.com/in/alexander-a-pan-444755151' target="_blank">
+              <v-btn icon small style="padding-right:18px; padding-left:8px;">
+                <v-icon>{{linkedInLogo}}</v-icon>
+              </v-btn>
+            </a>
+          </div>
+        </v-card-title>
+        <v-card-text class="w3-black py-2 white--text text-center">
+          <v-icon>
+            {{copyRightLogo}}
+          </v-icon>
+          {{ new Date().getFullYear() }} — <strong>Alexander Pan</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import {
-  mdiFacebook,
-  mdiInstagram,
-  mdiGithubCircle,
-  mdiLinkedin
-} from "@mdi/js";
+  import HelloWorld from "./components/HelloWorld.vue";
+  import {
+    mdiFacebook,
+    mdiInstagram,
+    mdiGithubCircle,
+    mdiLinkedin,
+    mdiCopyright,
+  } from "@mdi/js";
 
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      fbLogo: mdiFacebook,
-      igLogo: mdiInstagram,
-      gitHubLogo: mdiGithubCircle,
-      linkedInLogo: mdiLinkedin
-    };
-  }
-};
+  export default {
+    name: "app",
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        fbLogo: mdiFacebook,
+        igLogo: mdiInstagram,
+        gitHubLogo: mdiGithubCircle,
+        linkedInLogo: mdiLinkedin,
+        copyRightLogo: mdiCopyright,
+      };
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.w3-animate-zoom {
-  animation: animatezoom 2.7s;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  .w3-animate-zoom {
+    animation: animatezoom 2.7s;
+  }
 </style>
